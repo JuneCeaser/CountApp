726dbcd1dd4e8ec6be5d3956d59554a054bd8075
June Ceaser <161857185+JuneCeaser@users.noreply.github.com>
2025-01-21 14:54:47 +0530

    Initial commit - React calculator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import "./App.css";
+import { useState } from "react";
+
+function CounterApp() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrementCounter = () => {
+    setCounter((prevCounter) => prevCounter - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <header className="app-header">Simple Counter App</header>
+      <div className="counter-box">
+        <button className="counter-button" onClick={decrementCounter}>
+          -
+        </button>
+        <h2 className="counter-value">{counter}</h2>
+        <button className="counter-button" onClick={incrementCounter}>
+          +
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default CounterApp;
